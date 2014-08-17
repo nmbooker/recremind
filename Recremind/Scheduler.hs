0f56984d7ca5ae2daaf505b618ea9b1e287235e3
Nick Booker <NMBooker@gmail.com>
2014-08-17 19:52:16 +0100

    Port Scheduler.hs to use Data.Time instead  of Data.Hourglass

--- Recremind/Scheduler.hs
+++ Recremind/Scheduler.hs
@@ -7,14 +7,17 @@ module Recremind.Scheduler (
 
 -- This contains the action that will actually build the reminder and schedule it
 
-import Data.Hourglass (Duration(..), Hours(..), DateTime, timePrint, timeAdd)
+-- import Data.Hourglass (Duration(..), Hours(..), DateTime, timePrint, timeAdd)
+import Data.Time
+import System.Locale (defaultTimeLocale)
+import Data.DateTime (DateTime(..), addMinutes, formatDateTime)
 import Data.Int (Int64)
 
 data Reminder = Reminder {
         programName     :: String    -- ^ name of program
     ,   channel         :: String    -- ^ name of broadcast channel
-    ,   firstShowing    :: DateTime  -- ^ time of first showing
-    ,   timerPeriodDays :: Int64     -- ^ how far in advance we can set timer, in days
+    ,   firstShowing    :: LocalTime  -- ^ time of first showing
+    ,   timerPeriodDays :: Integer     -- ^ how far in advance we can set timer, in days
 } deriving (Show)
 
 
@@ -27,20 +30,23 @@ reminderBody :: Reminder
 reminderBody reminder = unlines $
     [   "Program:        " ++ (programName reminder)
     ,   "Channel:        " ++ (channel reminder)
-    ,   "When:           " ++ (timePrint "DD/MM/YYYY H:MI" $ firstShowing reminder)
+    ,   "When:           " ++ (formatTime defaultTimeLocale "%d/%m/%Y %H:%M" $ firstShowing reminder)
     ]
 
 
 whenToRemind :: Reminder 
-                -> DateTime     -- ^ when to send the reminder
+                -> LocalTime     -- ^ when to send the reminder
 whenToRemind reminder =
     (timerPeriodDays reminder) `daysBefore` (firstShowing reminder)
 
+addDaysToLocalTime :: Integer -> LocalTime -> LocalTime
+addDaysToLocalTime nDays time =
+    let newDay = addDays nDays (localDay time)
+    in time { localDay = newDay }
 
-numDays `daysBefore` time =
-    time `timeAdd` nullDuration {
-        durationHours = Hours ((- numDays) * hoursPerDay)
-    }
+d `daysBefore` t = addDaysToLocalTime (0 - d) t
 
 hoursPerDay = 24
-nullDuration = Duration 0 0 0 0
+minutesPerHour = 60
+minutesPerDay = minutesPerHour * hoursPerDay
+
