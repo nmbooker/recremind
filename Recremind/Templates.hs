a4283996ab968b0378390af13b90a9ddcbf12e53
Nick Booker <NMBooker@gmail.com>
2014-08-18 18:18:10 +0100

    Fix unused do bind (replacing mapM with mapM_)

--- Recremind/Templates.hs
+++ Recremind/Templates.hs
@@ -99,7 +99,7 @@ twitterBootstrap includeTheme = do
             ! A.rel "stylesheet"
             ! A.href "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css"
 
-        mapM preEscapedText [
+        mapM_ preEscapedText [
               "<!--[if lt IE 9]>"
             , "    <script src=\"https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js\"></script>"
             , "    <script src=\"https://oss.maxcdn.com/respond/1.4.2/respond.min.js\"></script>"
